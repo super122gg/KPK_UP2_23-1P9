f664fbc00fe10996937a359262ff5bee39b03ade
super122 <super122.05.122@gmail.com>
2026-05-17 17:06:24 +0300

    Update models.py

--- S24/models.py
+++ S24/models.py
@@ -4,13 +4,16 @@ db = SqliteDatabase('room_availability.db')
 class BaseModel(Model):
     class Meta:
         database = db
+
 class Room(BaseModel):
     number = CharField(unique=True, max_length=10)
     floor = IntegerField()
     capacity = IntegerField()
+
 class Event(BaseModel):
     title = CharField(max_length=100)
     type = CharField(max_length=50)
+
 class RoomBlock(BaseModel):
     room = ForeignKeyField(Room, backref='blocks', null=False, on_delete='CASCADE')
     event = ForeignKeyField(Event, backref='blocks', null=False, on_delete='CASCADE')
@@ -19,10 +22,12 @@ class RoomBlock(BaseModel):
     status = CharField(max_length=20, default='active')
     comment = TextField(default='')
     is_deleted = BooleanField(default=False)
+
 def init_db():
     db.connect()
     db.create_tables([Room, Event, RoomBlock], safe=True)
     db.close()
-    print("База данных и таблицы инициализированы.")
+    print("БД инициализирована")
+
 if __name__ == "__main__":
     init_db()
