6357cddc8ffa200b1b894887f6c522784f3e762e
super122 <super122.05.122@gmail.com>
2026-05-18 13:05:38 +0300

    Update models.py

--- S24/models.py
+++ S24/models.py
@@ -1,9 +1,14 @@
 from peewee import *
 
 db = SqliteDatabase('room_availability.db')
+
 class BaseModel(Model):
     class Meta:
         database = db
+
+class Status(BaseModel):
+    name = CharField(unique=True, max_length=20)  
+    description = CharField(max_length=100, null=True)
 
 class Room(BaseModel):
     number = CharField(unique=True, max_length=10)
@@ -17,15 +22,18 @@ class Event(BaseModel):
 class RoomBlock(BaseModel):
     room = ForeignKeyField(Room, backref='blocks', null=False, on_delete='CASCADE')
     event = ForeignKeyField(Event, backref='blocks', null=False, on_delete='CASCADE')
+    status = ForeignKeyField(Status, backref='blocks', null=False, on_delete='PROTECT')
     start_datetime = DateTimeField()
     end_datetime = DateTimeField()
-    status = CharField(max_length=20, default='active')
     comment = TextField(default='')
     is_deleted = BooleanField(default=False)
 
 def init_db():
     db.connect()
-    db.create_tables([Room, Event, RoomBlock], safe=True)
+    db.create_tables([Status, Room, Event, RoomBlock], safe=True)
+    default_statuses = ['active', 'cancelled', 'pending']
+    for s_name in default_statuses:
+        Status.get_or_create(name=s_name)
     db.close()
     print("БД инициализирована")
 
