18f626ec00a2b8e6d04b6baaa19a0a2d346bd2dc
super122 <super122.05.122@gmail.com>
2026-06-04 13:30:12 +0300

    Update models.py

--- S24/models.py
+++ S24/models.py
@@ -40,6 +40,17 @@ class RoomBlock(BaseModel):
             Check('end_datetime > start_datetime')
         ]
 
+    def save(self, *args, **kwargs):
+        # Приводим start_datetime к UTC
+        if self.start_datetime.tzinfo is None:
+            start = self.start_datetime.replace(tzinfo=timezone.utc)
+        else:
+            start = self.start_datetime.astimezone(timezone.utc)
+        # Проверка: start_datetime не может быть в прошлом
+        if start <= datetime.now(timezone.utc):
+            raise ValueError("start_datetime cannot be in the past")
+        return super().save(*args, **kwargs)
+
 def init_db(close_after: bool = False):
     """Создаёт таблицы, если их нет. Инициализация справочников выполняется в service.py."""
     try:
